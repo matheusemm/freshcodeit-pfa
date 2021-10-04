5ad1f4a043b67c90ac6e256eb72fe36218e804c9
Matheus Moreira <matheus.moreira@bayes.gg>
2021-10-04 13:39:11 +0200

    Add colors to 'costs per month' graph

--- src/freshcodeit_pfa/components/monthly_costs.cljs
+++ src/freshcodeit_pfa/components/monthly_costs.cljs
@@ -1,7 +1,9 @@
 (ns freshcodeit-pfa.components.monthly-costs
   (:require [reagent.core :as r]
             [goog.string :as gstr]
-            [goog.string.format]))
+            [goog.string.format]
+            ["chart.js" :refer [Chart]]
+            ["color-scheme" :as ColorScheme]))
 
 (defn costs-by-month
   [transactions]
@@ -11,19 +13,44 @@
           {}
           transactions))
 
+(defn hex-colors
+  []
+  (-> (ColorScheme.)
+      #_(.from_hue 21)
+      (.scheme "triade")
+      (.variation "soft")
+      (.colors)
+      (js->clj)))
+
+(defn chart-colors
+  []
+  (let [colors (map (fn [hex] (->> hex
+                                   (partition 2)
+                                   (map #(js/parseInt (apply str %) 16))))
+                    (hex-colors))]
+    {:borders (mapv #(apply gstr/format "rgb(%s, %s, %s)" %) colors)
+     :backgrounds (mapv #(apply gstr/format "rgba(%s, %s, %s, 0.2)" %) colors)}))
+
+
+
 (defn chart-data
   [costs]
-  (clj->js {:type "bar"
-            :data {:labels (vec (keys costs))
-                   :datasets [{:label "Total Cost"
-                               :data (vec (vals costs))}]}
-            :options {:scales {:y {:beginAtZero true}}}}))
+  (let [{:keys [borders backgrounds]} (chart-colors)]
+    (clj->js {:type "bar"
+              :data {:labels (vec (keys costs))
+                     :datasets [{:label "Total Cost"
+                                 :data (vec (vals costs))
+                                 :backgroundColor backgrounds
+                                 :borderColor borders
+                                 :borderWidth 1}]}
+              :options {:scales {:y {:beginAtZero true}}}})))
 
 (defn display-chart
   [transactions]
   (let [context (.getElementById js/document "monthly-costs-canvas")
-        data (-> transactions costs-by-month chart-data)]
-    (js/Chart. context data)))
+        data (-> transactions costs-by-month chart-data)
+        _ (.log js/console data)]
+    (Chart. context data)))
 
 (defn monthly-costs
   [transactions]
